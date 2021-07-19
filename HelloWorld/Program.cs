b64d62ca4dd7f9f8d058fb95fa938314daa03b8f
nashv <nashvegasnate@gmail.com>
2021-07-19 14:58:47 -0500

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Howdy Y'all!!");
+            Console.ReadKey();
+            //without this line^, you get an exit code 0
+
+            var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan" };
+
+            foreach (var animal in animals)
+            {
+                if (animal.Length >= 6)
+                    Console.WriteLine(animal);
+            }
+
+            Console.WriteLine("Choose a greeting style by entering the corresponding number");
+            Console.WriteLine("1 - Southern");
+            Console.WriteLine("2 - Hawaiian");
+            Console.WriteLine("3 - Swedish");
+
+            var chosenGreeting = Console.ReadLine();
+
+            switch (chosenGreeting)
+            {
+                case "1":
+                    Console.WriteLine("Howdy");
+                    break;
+
+                case "2":
+                    Console.WriteLine("Aloha");
+                    break;
+
+                case "3":
+                    Console.WriteLine("Hej Hej");
+                    break;
+
+                default:
+                    Console.WriteLine("Howdy");
+                    break;
+
+            }
+          
+        }
+    }
+}
